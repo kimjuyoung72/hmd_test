b4bb1dd3e6863c5cc475a97b673c7341ca20d16b
kimjuyoung <devbat@naver.com>
2022-09-28 13:28:14 +0900

    220928 13:30

--- src/main/java/com/kh/hanmadang/domain/dao/post/ReviewPostDAOImpl.java
+++ src/main/java/com/kh/hanmadang/domain/dao/post/ReviewPostDAOImpl.java
@@ -54,9 +54,28 @@ public class ReviewPostDAOImpl implements ReviewPostDAO{
 
     StringBuffer sql = new StringBuffer();
 
-    sql.append("select * ");
+    sql.append("select review_from_id, ");
+    sql.append("       review_post_id, ");
+    sql.append("       review_post_title, ");
+    sql.append("       review_post_link, ");
+    sql.append("       review_post_content, ");
+    sql.append("       review_post_writer, ");
+    sql.append("       cdate, ");
+    sql.append("       udate, ");
+    sql.append("       review_post_good ");
+//    sql.append("select * ");
     sql.append("  from review ");
     sql.append(" where review_post_id = ? ");
+
+//    private String reviewFromId;            //관련 공연 ID
+//    private Long reviewPostId;            //글번호 ID
+//    private String reviewPostTitle;         //제목
+//    private String reviewPostLink;          //홍보이미지 링크
+//    private StringBuffer reviewPostContent; //내용
+//    private String reviewPostWriter;        //작성자
+//    private LocalDateTime cdate;            //작성일
+//    private LocalDateTime udate;            //수정일
+//    private Integer reviewPostGood;         //좋아요 -_-)b
 
     Review findedReview = null;
     try {
